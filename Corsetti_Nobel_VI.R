8c052573cb8d7c48ee29cd3cb7d89e79ec1eb182
leacorsetti <35219287+leacorsetti@users.noreply.github.com>
2018-01-18 17:10:29 +0100

    Update Corsetti_Nobel_VI.R

--- Corsetti_Nobel_VI.R
+++ Corsetti_Nobel_VI.R
@@ -1,3 +1,17 @@
-library('tidyverse')
+#drawing the final graphs which includes frequency
 
-nobel-geo <-
+library('tidyverse')
+library('data.table')
+Nobel <- read.csv('nobel_coordinates.csv')
+install.packages("ggthemes")
+frequency <- read.csv("country_and_frequency.csv")
+library(ggthemes)
+library(ggmap)
+library(maps)
+library(mapdata)
+
+#plotting correct final version of map with correct data set
+ggplot(data=frequency) + theme_tufte() +
+  geom_polygon(data = map_data('world'), mapping = aes(x = long, y = lat, group=group), fill='grey', colour='white') +
+  geom_point(data=frequency, mapping = aes(x=lon, y=lat, size= nationality_freq, colour= nationality_label), alpha = 0.9)
+ 
